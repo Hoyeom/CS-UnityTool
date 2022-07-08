55397f63fb39d99ac9e37f1cc4760cad3c32d8f9
Hoyeom <korehdrb@gmail.com>
2022-07-08 14:38:57 +0900

    Color Picker

--- UnityTool/Assets/Scripts/ToolKitLearn.cs
+++ UnityTool/Assets/Scripts/ToolKitLearn.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class ToolKitLearn : EditorWindow
 {
+
+    private Color _copyColor = Color.black;
+    
     [MenuItem("QuickTool/Open")]
     private static void ShowWindow()
     {
@@ -24,13 +29,37 @@ public class ToolKitLearn : EditorWindow
 
         rootVisualElement.Add(container);
 
-        Label title = new Label("UI Elements");
-        Label biggerTitle = new Label("Bigger Title");
+        Label title = new Label("Color Picker");
+        ColorField colorField = new ColorField();
         
-        container.Add(biggerTitle);
         container.Add(title);
+        container.Add(colorField);
 
+        VisualElement buttonsContainer = new VisualElement();
 
+        Button randomColorButton =  CreateButton("Random Color",()=>colorField.value = RandomColor());
+        Button resetColorButton = CreateButton("Reset Color",()=>colorField.value = Color.black);
+        Button copyColorButton = CreateButton("Copy Color",()=>  _copyColor = colorField.value);;
+        Button pasteColorButton = CreateButton("Paste Color", () => colorField.value = _copyColor);
 
+        buttonsContainer.Add(randomColorButton);
+        buttonsContainer.Add(resetColorButton);
+        buttonsContainer.Add(copyColorButton);
+        buttonsContainer.Add(pasteColorButton);
+        
+        container.Add(buttonsContainer);
     }
+
+    private Button CreateButton(string text,Action action = null)
+    {
+        Button button = new Button() {text = text};
+        button.clicked += action;
+        return button;
+    }
+
+    private Color RandomColor()
+    {
+        return Random.ColorHSV(0, 1, 0, 1, 0, 1, 1, 1);
+    }
+    
 }
